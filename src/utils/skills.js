a9a273aa1140ebc9af89632bec1ea88a48bf6831
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-24 21:37:09 +0900

    change the information is done

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,6 +1,7 @@
 import { ReactComponent as Down } from "../assets/icons/angle-down.svg"
 import { ReactComponent as MyReact } from "../assets/icons/react.svg"
 import { ReactComponent as Library } from "../assets/icons/swatchbook.svg"
+import { ReactComponent as Database } from "../assets/icons/database.svg"
 import { ReactComponent as CurlyBraces } from "../assets/icons/brackets-curly.svg"
 
 export const obj = [
@@ -9,27 +10,34 @@ export const obj = [
         Icon: CurlyBraces,
         title: "Programming Languages",
         arrow: Down,
-        skills: ["HTML", "CSS", "Java Script", "Java"]
+        skills: ["Java Script", "Java", "Nodejs"]
     },
     {
         id: 1,
         Icon: MyReact,
         title: "React",
         arrow: Down,
-        skills: ["Hooks", "Styled Components", "Rest API"]
+        skills: ["Hooks", "Styled Components", "Rest API", "React Table", "React Form", "React Helmet"]
     },
     {
         id: 1,
         Icon: Library,
         title: "Libraries",
         arrow: Down,
-        skills: ["Google Maps", "Ant Design", "Recharts", "Bootstrap"]
+        skills: ["Google Maps", "Recharts", "Bootstrap", "Tailwind"]
+    },
+    {
+        id: 1,
+        Icon: Database,
+        title: "Backend",
+        arrow: Down,
+        skills: ["nodejs", "Expressjs", "mongodb"]
     },
     {
         id: 1,
         Icon: CurlyBraces,
         title: "Others",
         arrow: Down,
-        skills: ["Figma", "Github", "Data Structure", "So on"]
+        skills: ["Figma", "Github", "Data Structure", "Recharts", "So on"]
     },
 ]
